e7e3b2d9eb03168ec9b9ff3926368f938f6f563c
david Maser <david.maser@altitude-sports.com>
2015-09-23 15:34:37 -0400

    REFACTOR

--- pgd/clone/js/clone-form-td.js
+++ pgd/clone/js/clone-form-td.js
@@ -28,7 +28,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 $(function () {
-    $('body').on('click','#btnAdd',function () {
+    $('body').on('click','.btnAdd',function () {
 
         var num     = $('.clonedInput').length, // Checks to see how many "duplicatable" input fields we currently have
             newNum  = new Number(num + 1),      // The numeric ID of the new input field being added, increasing by 1 each time
@@ -80,7 +80,7 @@ $(function () {
 
     // Right now you can only add 4 sections, for a total of 5. Change '5' below to the max number of sections you want to allow.
         if (newNum == 5)
-        $('#btnAdd').attr('disabled', true).prop('value', "You've reached the limit"); // value here updates the text in the 'add' button when the limit is reached
+        $('.btnAdd').attr('disabled', true).prop('value', "You've reached the limit"); // value here updates the text in the 'add' button when the limit is reached
         $('.date_obj').datetimepicker({format: 'DD/MM/YYYY HH:mm'});
     });
     $('body').on('click','.overlay_close',function(){
@@ -157,7 +157,7 @@ $(function () {
         $("html, body").animate({ scrollTop: 0 }, "slow");
     }
 
-    $('#btnDel').click(function () {
+    $('.btnDel').click(function () {
     // Confirmation dialog box. Works on all desktop browsers and iPhone.
         if (confirm("Are you sure you wish to remove this section? This cannot be undone."))
             {
@@ -166,13 +166,13 @@ $(function () {
                 $('#entry' + num).slideUp('slow', function () {$(this).remove();
                 // if only one element remains, disable the "remove" button
                     if (num -1 === 1)
-                $('#btnDel').attr('disabled', true);
+                $('.btnDel').attr('disabled', true);
                 // enable the "add" button
-                $('#btnAdd').attr('disabled', false).prop('value', "add section");});
+                $('.btnAdd').attr('disabled', false).prop('value', "add section");});
             }
         return false; // Removes the last section you added
     });
-    $('#submit_button').click(function (){
+    $('.submit_json').click(function (){
         var c = [];
         var len = $('.clonedInput form').length;
         for(var i=0;i<len;i++){
@@ -182,7 +182,7 @@ $(function () {
         outputJson(c);
     });
     // Enable the "add" button
-    $('#btnAdd').attr('disabled', false);
+    $('.btnAdd').attr('disabled', false);
     // Disable the "remove" button
-    $('#btnDel').attr('disabled', true);
+    $('.btnDel').attr('disabled', true);
 });
