ddae438c3c705814ba73c473750530cd900cd04e
david Maser <david.maser@altitude-sports.com>
2015-09-23 16:55:11 -0400

    Translate JSON object

--- pgd/clone/js/clone-form-td.js
+++ pgd/clone/js/clone-form-td.js
@@ -28,18 +28,17 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 $(function () {
-    $('body').on('click','.btnAdd',function () {
-
+    function addItems(){
         var num     = $('.clonedInput').length, // Checks to see how many "duplicatable" input fields we currently have
             newNum  = new Number(num + 1),      // The numeric ID of the new input field being added, increasing by 1 each time
-            newElem = $('#entry' + num).clone().attr('id', 'entry' + newNum).fadeIn('slow'); // create the new element via clone(), and manipulate it's ID using newNum value
-    
-    /*  This is where we manipulate the name/id values of the input inside the new, cloned element
-        Below are examples of what forms elements you can clone, but not the only ones.
-        There are 2 basic structures below: one for an H2, and one for form elements.
-        To make more, you can copy the one for form elements and simply update the classes for its label and input.
-        Keep in mind that the .val() method is what clears the element when it gets cloned. Radio and checkboxes need .val([]) instead of .val('').
-    */
+            newElem = $('#entry' + num).clone().attr('id', 'entry' + newNum); // create the new element via clone(), and manipulate it's ID using newNum value
+
+        /*  This is where we manipulate the name/id values of the input inside the new, cloned element
+         Below are examples of what forms elements you can clone, but not the only ones.
+         There are 2 basic structures below: one for an H2, and one for form elements.
+         To make more, you can copy the one for form elements and simply update the classes for its label and input.
+         Keep in mind that the .val() method is what clears the element when it gets cloned. Radio and checkboxes need .val([]) instead of .val('').
+         */
         // H2 - section
         newElem.find('.heading-reference').attr('id', 'ID' + newNum + '_reference').attr('name', 'ID' + newNum + '_reference').html('<span class="label label-default">HERO ITEM ' + newNum+'</span>');
 
@@ -71,20 +70,23 @@ $(function () {
         newElem.find('.label_twt').attr('for', 'ID' + newNum + '_twitter_handle');
         newElem.find('.input_twt').attr('id', 'ID' + newNum + '_twitter_handle').attr('name', 'ID' + newNum + '_twitter_handle').val('');
 
-    // Insert the new element after the last "duplicatable" input field
+        // Insert the new element after the last "duplicatable" input field
         $('#entry' + num).after(newElem);
         $('#ID' + newNum + '_title').focus();
 
-    // Enable the "remove" button. This only shows once you have a duplicated section.
+        // Enable the "remove" button. This only shows once you have a duplicated section.
         $('#btnDel').attr('disabled', false);
 
-    // Right now you can only add 4 sections, for a total of 5. Change '5' below to the max number of sections you want to allow.
+        // Right now you can only add 4 sections, for a total of 5. Change '5' below to the max number of sections you want to allow.
         if (newNum == 10)
-        $('.btnAdd').attr('disabled', true).prop('value', "You've reached the limit"); // value here updates the text in the 'add' button when the limit is reached
+            $('.btnAdd').attr('disabled', true).prop('value', "You've reached the limit"); // value here updates the text in the 'add' button when the limit is reached
         $('.date_obj').datetimepicker({format: 'DD/MM/YYYY HH:mm'});
         $('html, body').animate({
             scrollTop: $('#entry' + newNum).offset().top
         }, 500);
+    }
+    $('body').on('click','.btnAdd',function () {
+        addItems();
     });
     $('body').on('click','.overlay_close',function(){
         $(this).parent().parent().hide();
@@ -102,9 +104,17 @@ $(function () {
     function traverseJSON(){
         if($('.blackify_overlay textarea').val() !== '') {
             var ctc = $('.blackify_overlay textarea').val(),
-                traverse = JSON.parse(ctc),
-                obj = traverse.hero,
-                len = obj.length;
+                prs = JSON.parse(ctc),
+                obj = prs.hero,
+                len = obj.length,
+                formItems = $('.clonedInput').length;
+            if(formItems < len){
+                var build = true,
+                    bItems = len-formItems;
+            }
+            for(var h = 0;h<bItems;h++){
+                addItems();
+            }
             for (var i = 0; i < len; i++) {
                 console.log(obj[i]);
             }
